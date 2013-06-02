f7c25b5c9ea56d0ba4d6bbcde8902d0bf1a0ef0d
Robert Colin <cfflymolo@gmail.com>
2013-06-01 17:45:31 -0700

    Created basic type picker for submission screen

--- iOS/UrbanFruitly/UrbanFruitly/Controllers/UFPostTableViewController.m
+++ iOS/UrbanFruitly/UrbanFruitly/Controllers/UFPostTableViewController.m
@@ -8,10 +8,17 @@
 
 #import "UFPostTableViewController.h"
 
-@interface UFPostTableViewController ()
+#define TYPE_PICKERVIEW_HEIGHT 180
+
+@interface UFPostTableViewController () <UIPickerViewDataSource,UIPickerViewDelegate>
+{
+    CGRect originalPickerFrame;
+}
 @property (weak, nonatomic) IBOutlet UIImageView *productImageView;
 @property (weak, nonatomic) IBOutlet UITextField *typeTextField;
-@property (weak, nonatomic) IBOutlet UIPickerView *pickerView;
+
+@property (strong, nonatomic) NSArray *productTypes;
+@property (strong, nonatomic) UIPickerView *typePickerView;
 @end
 
 @implementation UFPostTableViewController
@@ -20,9 +27,15 @@
 {
     [super viewDidLoad];
     
-    self.pickerView = [[UIPickerView alloc] init];
+    originalPickerFrame = CGRectMake(0, UIScreen.mainScreen.bounds.size.height, UIScreen.mainScreen.bounds.size.width ,TYPE_PICKERVIEW_HEIGHT);
+    
+    self.typePickerView = [[UIPickerView alloc] initWithFrame:originalPickerFrame];
+    self.typePickerView.dataSource = self;
+    self.typePickerView.delegate = self;
+    [self.view addSubview:self.typePickerView];
+    
+    self.productTypes = @[@"Apple", @"Oranges", @"Avacado"];
     self.typeTextField.enabled = NO;
-    self.typeTextField.inputView = self.pickerView;
 }
 
 - (IBAction)changeTypeOfProduct
@@ -33,8 +46,35 @@
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
     if (indexPath.section == 1 && indexPath.row == 0) {
-        NSLog(@"SelectingType");
+        [UIView animateWithDuration:0.5 animations:^{
+            CGRect frame = self.typePickerView.frame;
+            self.typePickerView.frame = CGRectMake(frame.origin.x, frame.origin.y-280, UIScreen.mainScreen.bounds.size.width, TYPE_PICKERVIEW_HEIGHT);
+        }];
     }
+}
+
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
+{
+    return 1;
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+{
+    return self.productTypes.count;
+}
+
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
+{
+    return self.productTypes [row];
+}
+
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component
+{
+    [UIView animateWithDuration:0.5f animations:^{
+        self.typePickerView.frame = originalPickerFrame;
+    }];
+    
+    self.typeTextField.text = self.productTypes [row];
 }
 
 @end
